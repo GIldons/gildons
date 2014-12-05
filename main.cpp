00f776ba77f22e40aa9f039ddb3abeae522441cc
Gildons <raulgildons@gmail.com>
2014-12-05 17:32:53 -0600

    te

--- main.cpp
+++ main.cpp
@@ -56,15 +56,15 @@ int main()
 	
 	turn_onoff(1);
 
-	send_output(fd, "+");
-	send_output(fd, "SF,1");
-	send_output(fd, "SS,C0000000");
-	send_output(fd, "SR,92000000");
-	send_output(fd, "R,1");
+	send_output(fd, "+\n");
+	send_output(fd, "SF,1\n");
+	send_output(fd, "SS,C0000000\n");
+	send_output(fd, "SR,92000000\n");
+	send_output(fd, "R,1\n");
 	SDL_Delay(250);
-	send_output(fd, "F");
+	send_output(fd, "F\n");
 	SDL_Delay(250);
-	send_output(fd, "X");
+	send_output(fd, "X\n");
 	
 	screen_time = spreed_time = SDL_GetTicks();
 	while(event.type != SDL_QUIT && exit)
