0c973e75afda6aa4202e7114f7ba480bb6e47f68
Gildons <raulgildons@gmail.com>
2014-12-08 11:07:26 -0600

    Beta 5

--- screen_opt.c
+++ screen_opt.c
@@ -37,9 +37,9 @@ void screen_text(SDL_Surface ** Surfaces, int ** dados, TTF_Font ** font)
 	stemp = TTF_RenderUTF8_Blended(font[0], temp, textColor);
 	apply_surface(10, 355, stemp, Surfaces[0], NULL);
 	SDL_FreeSurface(stemp);
-	if(*dados[2] > 30)
+	if(*(dados[2]) > 30)
 		sprintf(temp, "Water Level: %d", *(dados[2]));
-	else if(*dados[2] > 0)
+	else if(*(dados[2]) > 0)
 		sprintf(temp, "Water Level: %d - Critical", *(dados[2]));
 	else
 		sprintf(temp, "No water! Go to base!");
@@ -135,9 +135,10 @@ void mouse_events(SDL_Event * event, int ** dados, Spot table[][8], int teste_mo
 						table[i][j].fire_lvl = 0;
 						if( *(dados[0]) <= 36 )
 						{
-							*(dados[0])=(*dados[0]) - 1;
-							if(*dados[2] > 0)
-								*(dados[2])=(*dados[2]) - 10;
+							*(dados[0])=*(dados[0]) - 1;
+							*(dados[1])=*(dados[1]) + 1;
+							if(*(dados[2]) > 0)
+								*(dados[2])=*(dados[2]) - 10;
 						}
 					}
 				}
