a895c042158e0d9a9e0b71abc042835762bc369e
Gildons <raulgildons@gmail.com>
2014-12-07 21:10:26 -0600

    fsdfds

--- I_O.c
+++ I_O.c
@@ -163,8 +163,10 @@ int send_output(int fd, char * data)
 
 void openBluetooh(int fd)
 {
+	char * t;
 	SDL_Delay(30);
 	send_output(fd, "+\n");
+	get_input(fd, t);
 	SDL_Delay(1);
 	send_output(fd, "SF,1\n");
 	SDL_Delay(1);
